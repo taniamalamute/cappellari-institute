12f1489c81f63081dafa1d1178e197b90124276b
marinasilviafinetti <marina.silvia.finetti@hotmail.it>
2017-06-27 12:15:17 +0200

    Corrected Model

--- Barcode check-digit calculator/FM/BarcodeCheckDigitCalculator.dpr
+++ Barcode check-digit calculator/FM/BarcodeCheckDigitCalculator.dpr
@@ -5,7 +5,6 @@ uses
   ViewCheckDigitCalulator in 'Views\ViewCheckDigitCalulator.pas' {Form1},
   InterfaceCheckDigitCalculator in 'Services\InterfaceCheckDigitCalculator.pas',
   PresenterCheckDigitCalculator in 'Presenters\PresenterCheckDigitCalculator.pas',
-  DataModuleCheckDigitCalculator in 'Services\DataModuleCheckDigitCalculator.pas' {DataModuleCheckDigit: TDataModule},
   ModelCheckDigitCalculator in 'Models\ModelCheckDigitCalculator.pas';
 
 {$R *.res}
@@ -14,6 +13,5 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TForm1, Form1);
-  Application.CreateForm(TDataModuleCheckDigit, DataModuleCheckDigit);
   Application.Run;
 end.
